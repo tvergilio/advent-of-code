0c047988f41cfbb472360fb4121bc28462fd30ab
thalita <t.vergilio@leedsbeckett.ac.uk>
2020-12-01 13:15:00 +0000

    first commit

---
+++ src/main/java/TwentyTwentyFinder.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TwentyTwentyFinder {
+    public static void main(String[] args) throws IOException {
+        Path source = Paths.get("D:\\projects\\advent-of-code\\target\\classes\\input");
+        List<Integer> numbers = Files.lines(source).map(Integer::valueOf).collect(Collectors.toList());
+        int answer = findProductOfThreeNumbers(numbers);
+        System.out.println(answer);
+    }
+
+    private static int findProductOfTwoNumbers(List<Integer> numbers) throws IOException {
+        return numbers.stream()
+                .filter(x -> numbers.stream().anyMatch(y -> x + y == 2020))
+                .reduce((x, y) -> x * y)
+                .get();
+    }
+
+    private static int findProductOfThreeNumbers(List<Integer> numbers) throws IOException {
+        return numbers.stream()
+                .filter(x -> numbers.stream()
+                        .anyMatch(y -> numbers.stream()
+                                .anyMatch(z -> x + y + z == 2020)))
+                .reduce((a, b) -> a * b)
+                .get();
+    }
+}
