de36464e4ec8f1ed08016d1bfe481ec6244c3be3
thalita <t.vergilio@leedsbeckett.ac.uk>
2020-12-04 14:40:51 +0000

    Completed part 2 of day 4

--- src/main/java/Week3.java
+++ src/main/java/Week3.java
@@ -12,11 +12,11 @@ public class Week3 {
 
     public static void main(String[] args) throws IOException {
         Path source = Paths.get("target\\classes\\passports");
-        Long result = getMatchesFirstRule(source);
+        Long result = getMatches(source);
         System.out.println(result);
     }
 
-    private static Long getMatchesFirstRule(Path source) throws IOException {
+    private static Long getMatches (Path source) throws IOException {
         return Arrays.stream(Files.readString(source)
                 .split(lineSeparator + lineSeparator))
                 .map(entry -> Arrays.stream(entry.split("[\\n\\r\\s]+")))
@@ -24,7 +24,56 @@ public class Week3 {
                     return stringStream.collect(Collectors.toMap(x -> x.substring(0, x.indexOf(':')), y -> y.substring(y.indexOf(':') + 1)));
                 })
                 .filter(map -> map.keySet().containsAll(requiredFields))
+                .filter(map -> validateBirthYear(map.get("byr")))
+                .filter(map -> validateIssueYear(map.get("iyr")))
+                .filter(map -> validateExpirationYear(map.get("eyr")))
+                .filter(map -> validateHeight(map.get("hgt")))
+                .filter(map -> validateHairColour(map.get("hcl")))
+                .filter(map -> validateEyeColour(map.get("ecl")))
+                .filter(map -> validatePassport(map.get("pid")))
                 .count();
+    }
+
+    private static boolean validateYear(String input, int length, int atLeast, int atMost) {
+        return input.length() >= length
+                && Integer.parseInt(input) >= atLeast
+                && Integer.parseInt(input) <= atMost;
+    }
+
+    private static boolean validateBirthYear(String birthYear) {
+        return validateYear(birthYear, 4, 1920, 2002);
+    }
+
+    private static boolean validateIssueYear(String issueYear) {
+        return validateYear(issueYear, 4, 2010, 2020);
+    }
+
+    private static boolean validateExpirationYear(String expirationYear) {
+        return validateYear(expirationYear, 4, 2020, 2030);
+    }
+
+    private static boolean validateHeight(String height) {
+        if (height.endsWith("cm")) {
+            int heightInCm = Integer.parseInt(height.substring(0, height.indexOf("cm")));
+            return heightInCm >= 150 && heightInCm <= 193;
+        } else if (height.endsWith("in")) {
+            int heightInCm = Integer.parseInt(height.substring(0, height.indexOf("in")));
+            return heightInCm >= 59 && heightInCm <= 76;
+        }
+        return false;
+    }
+
+    private static boolean validateHairColour(String hairColour) {
+        return hairColour.matches("^#[0-9a-fA-F]{6}");
+    }
+
+    private static boolean validateEyeColour(String eyeColour) {
+        List<String> validColours = Arrays.asList("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+        return validColours.contains(eyeColour);
+    }
+
+    private static boolean validatePassport(String passportNumber) {
+        return passportNumber.matches("[0-9]{9}");
     }
 }
 
